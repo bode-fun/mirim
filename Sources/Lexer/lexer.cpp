3a21ce7c2fe5f2da97a7c8edf80a3ac7c972f24a
Jan Fooken <git@bode.fun>
2023-09-16 18:18:34 +0200

    Split Lexer into header and translation unit

--- Sources/Lexer/lexer.cpp
+++ Sources/Lexer/lexer.cpp
@@ -1,71 +1,44 @@
-#include "token.hpp"
-
-#include <cstdint>
-#include <string>
-#include <string_view>
+#include "lexer.hpp"
 
 namespace Mirim {
 namespace Lexer {
 
-/**
- * A cursor based lexer, which extracts tokens from a given input.
- */
-class Lexer {
-private:
-  std::uint64_t cursor = 0;
-  std::string_view input;
+Lexer::Lexer(std::string_view input) : cursor(0), input(input) {}
 
-  /**
-   * Returns the current character and bumps the cursor.
-   * If there is no character left, it returns '\0' as EOF.
-   */
-  auto bump() -> char {
-    if (this->cursor < this->input.length()) {
-      auto current_char = this->input.at(this->cursor);
-      this->cursor += 1;
-      return current_char;
-    }
-
-    return '\0';
+auto Lexer::bump() -> char {
+  if (this->cursor < this->input.length()) {
+    auto current_char = this->input.at(this->cursor);
+    this->cursor += 1;
+    return current_char;
   }
 
-  /**
-   * Returns the next character from the input without incrementing the cursor.
-   */
-  auto peek() const -> char {
-    if (this->cursor < this->input.length() - 1) {
-      auto next_char = this->input.at(this->cursor + 1);
-      return next_char;
-    }
+  return '\0';
+}
 
-    return '\0';
+auto Lexer::peek() const -> char {
+  if (this->cursor < this->input.length() - 1) {
+    auto next_char = this->input.at(this->cursor + 1);
+    return next_char;
   }
 
-  /**
-   * Moves the lexer's cursor until a character is reached.
-   */
-  auto eat_whitespace() -> void {
-    while (this->is_whitespace(this->peek())) {
-      this->bump();
-    }
-  }
+  return '\0';
+}
 
-  /**
-   * Determins if the given character is whitespace.
-   */
-  static auto is_whitespace(char input) -> bool {
-    switch (input) {
-    case ' ':
-    case '\n':
-      return true;
-    default:
-      return false;
-    }
+auto Lexer::eat_whitespace() -> void {
+  while (this->is_whitespace(this->peek())) {
+    this->bump();
   }
+}
 
-public:
-  Lexer(std::string_view input) : input(input) {}
-};
+auto Lexer::is_whitespace(char input) const -> bool {
+  switch (input) {
+  case ' ':
+  case '\n':
+    return true;
+  default:
+    return false;
+  }
+}
 
 } // namespace Lexer
 } // namespace Mirim
