4dfba309911ccc1ca3cde08e86f74ad2774762e7
Jan Fooken <git@bode.fun>
2023-09-22 18:29:13 +0200

    Add all missing tokens

--- Sources/Lexer/token.hpp
+++ Sources/Lexer/token.hpp
@@ -1,20 +1,122 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 
 namespace Mirim {
 namespace Lexer {
 
-enum TokenKind {
-  illegal,
-  eof,
+enum TokenKind : int8_t {
+  // Special Tokens
+  Illegal,
+  Eof,
+  Lf,
+  Whitespace,
+  Comment,
+  // End Special Tokens
 
-  identifier,
+  // Literals
+  Ident,  // main
+  Int,    // 1234
+  Float,  // 12.34
+  Char,   // 'a'
+  String, // "abc"
+          // TODO: MultilineString // """abc"""
+          // TODO: FormatString // `abc ${indent}`
+  // End Literals
 
-  keyword,
+  // Operators and Delimiters
+  Plus,      // +
+  PlusEqual, // +=
 
-  lParenthesis,
-  rParenthesis,
+  Minus,      // -
+  MinusEqual, // -=
+
+  Star,      // *
+  StarEqual, // *=
+
+  Slash,      // /
+  SlashEqual, // /=
+
+  Percent,      // %
+  PercentEqual, // %=
+
+  Equal,      // =
+  EqualEqual, // ==
+
+  Bang,      // !
+  BangEqual, // !=
+
+  Ampersand,          // &
+  AmpersandEqual,     // &=
+  AmpersandAmpersand, // &&
+
+  Pipe,      // |
+  PipeEqual, // |=
+  PipePipe,  // ||
+
+  Caret,      // ^
+  CaretEqual, // ^=
+
+  AngleBracketLeft,                  // <
+  AngleBracketAngleBracketLeft,      // <<
+  AngleBracketLeftEqual,             // <=
+  AngleBracketAngleBracketLeftEqual, // <<=
+
+  AngleBracketRight,                  // >
+  AngleBracketAngleBracketRight,      // >>
+  AngleBracketRightEqual,             // >=
+  AngleBracketAngleBracketRightEqual, // >>=
+
+  Ellipsis,     // ...
+  Comma,        // ,
+  Period,       // .
+  Colon,        // :
+  Pound,        // #
+  QuestionMark, // ?
+
+  LParen, // (
+  LBrack, // [
+  LBrace, // {
+
+  RParen, // )
+  RBrack, // ]
+  RBrace, // }
+  // End Operators and Delimiters
+
+  // Keywords
+  Alias, // alias
+  Await, // await
+  Async, // async
+  Break, // break
+
+  Case,     // case
+  Catch,    // catch
+  Continue, // continue
+  Default,  // default
+  Defer,    // defer
+
+  Else, // else
+  Enum, // enum
+  For,  // for
+  Fun,  // fun
+
+  If,        // if
+  Import,    // import
+  Interface, // interface
+  Let,       // let
+
+  Of,     // of
+  Pub,    // pub
+  Return, // return
+
+  Struct, // struct
+  Switch, // switch
+  Throws, // throws
+  Try,    // try
+
+  Var, // var
+  // End Keywords
 };
 
 /**
