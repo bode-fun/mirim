081ddc2da1f15e28597301b55615487e6cb91b5f
Jan Fooken <git@bode.fun>
2023-09-23 19:02:20 +0200

    Tokenize single char tokens.

--- Sources/Tokenizer/tokenizer.cpp
+++ Sources/Tokenizer/tokenizer.cpp
@@ -18,8 +18,66 @@ Tokenizer::Tokenizer(std::string_view input) : source(input) {
 }
 
 auto Tokenizer::next() -> Token {
-  auto tok = Token(TokenKind::Eof, "");
-  return tok;
+  if (index >= source.length()) {
+    return Token(TokenKind::Eof);
+  }
+
+  auto ch = source.at(index);
+
+  index += 1;
+
+  // A switch doesn't seem to work.
+  if (ch == 0) {
+    return Token(TokenKind::Illegal);
+  } else if (ch == '+') {
+    return Token(TokenKind::Plus);
+  } else if (ch == '-') {
+    return Token(TokenKind::Minus);
+  } else if (ch == '*') {
+    return Token(TokenKind::Star);
+  } else if (ch == '/') {
+    return Token(TokenKind::Slash);
+  } else if (ch == '%') {
+    return Token(TokenKind::Percent);
+  } else if (ch == '=') {
+    return Token(TokenKind::Equal);
+  } else if (ch == '!') {
+    return Token(TokenKind::Bang);
+  } else if (ch == '&') {
+    return Token(TokenKind::Ampersand);
+  } else if (ch == '|') {
+    return Token(TokenKind::Pipe);
+  } else if (ch == '^') {
+    return Token(TokenKind::Caret);
+  } else if (ch == '<') {
+    return Token(TokenKind::AngleBracketLeft);
+  } else if (ch == '>') {
+    return Token(TokenKind::AngleBracketRight);
+  } else if (ch == ',') {
+    return Token(TokenKind::Comma);
+  } else if (ch == '.') {
+    return Token(TokenKind::Period);
+  } else if (ch == ':') {
+    return Token(TokenKind::Colon);
+  } else if (ch == '#') {
+    return Token(TokenKind::Pound);
+  } else if (ch == '?') {
+    return Token(TokenKind::QuestionMark);
+  } else if (ch == '(') {
+    return Token(TokenKind::LParen);
+  } else if (ch == '[') {
+    return Token(TokenKind::LBrack);
+  } else if (ch == '{') {
+    return Token(TokenKind::LBrace);
+  } else if (ch == ')') {
+    return Token(TokenKind::RParen);
+  } else if (ch == ']') {
+    return Token(TokenKind::RBrack);
+  } else if (ch == '}') {
+    return Token(TokenKind::RBrace);
+  } else {
+    assert(false && "Token not implemented");
+  }
 }
 
 } // namespace Tokenizer
--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,11 +1,13 @@
 #include "Tokenizer/token.hpp"
 #include "Tokenizer/tokenizer.hpp"
-#include <string>
 
 #include "catch2/catch_test_macros.hpp"
-#include <catch2/catch_all.hpp>
 
-TEST_CASE("Tokenizer skips BOM") {
+#include <cstdint>
+#include <string>
+#include <vector>
+
+TEST_CASE("Tokenizer skips BOM at the beginning") {
   auto input = std::string();
   input.push_back(0xEF);
   input.push_back(0xBB);
@@ -17,4 +19,59 @@ TEST_CASE("Tokenizer skips BOM") {
   auto tok = tokenizer.next();
 
   REQUIRE(tok.kind == Mirim::Tokenizer::TokenKind::Eof);
+}
+
+TEST_CASE("Tokenizer sees 0 byte as an illegal token") {
+  auto input = std::string();
+  input.push_back(0);
+
+  auto tokenizer = Mirim::Tokenizer::Tokenizer(input);
+  auto tok = tokenizer.next();
+
+  REQUIRE(tok.kind == Mirim::Tokenizer::TokenKind::Illegal);
+}
+
+TEST_CASE("Tokenizer tokenizes single char operators correctly") {
+  std::string input = "+-*/%=!&|^<>,.:#?([{)]}";
+
+  auto tokenizer = Mirim::Tokenizer::Tokenizer(input);
+
+  std::vector<Mirim::Tokenizer::Token> expected_tokens = {
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Plus),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Minus),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Star),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Slash),
+
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Percent),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Equal),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Bang),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Ampersand),
+
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Pipe),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Caret),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::AngleBracketLeft),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::AngleBracketRight),
+
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Comma),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Period),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Colon),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Pound),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::QuestionMark),
+
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::LParen),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::LBrack),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::LBrace),
+
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::RParen),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::RBrack),
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::RBrace),
+
+      Mirim::Tokenizer::Token(Mirim::Tokenizer::TokenKind::Eof),
+  };
+
+  for (uint32_t i = 0; i <= input.length(); i++) {
+    auto provided = tokenizer.next();
+    auto expected = expected_tokens.at(i).kind;
+    REQUIRE(provided.kind == expected);
+  }
 }
